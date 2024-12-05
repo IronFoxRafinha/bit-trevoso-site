f95fd6656c5be936d9830679028f596e2dcf723e
Everton Misiagia <eamissiagia@gmail.com>
2024-12-05 16:38:45 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Uma Jornada Pela História dos Jogos de Terror';
+
+function curiosidadeDoDia() {
+    alert('A cidade de Silent Hill, com sua névoa constante e atmosfera opressiva, foi inspirada em Centralia, uma cidade na Pensilvânia que sofreu com um incêndio subterrâneo por décadas. A cidade real foi lentamente abandonada pelos moradores devido aos gases tóxicos e à instabilidade do solo.');
+};
